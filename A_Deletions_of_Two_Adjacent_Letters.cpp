c51228cf44e9b509ff5aa5dd48a7416c072826b0
jupyterNauticals <115215622+jupyterNauticals@users.noreply.github.com>
2022-10-08 19:30:43 +0530

    solution to a codechef problem

---
+++ A_Deletions_of_Two_Adjacent_Letters.cpp
@@ -0,0 +1,36 @@
+//author:Chinmay Lohani
+//date:9-3-2022
+//problemName:A. Deletions of Two Adjacent Letters
+//problemLink:https://codeforces.com/contest/1650/problem/A
+//memoryLimit: 256
+//timeLimit:2000
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define mod 998244353
+#define lli long long int
+#define ll long long
+#define li long int
+#define FastIO               \
+    ios::sync_with_stdio(0); \
+    cin.tie(0)
+
+void solve();
+int main()
+{
+    FastIO;
+    ll tc;
+    cin >> tc;
+    while (tc--)
+    {
+        solve();
+    }
+    return 0;
+}
+void solve()
+{
+
+    return;
+}
